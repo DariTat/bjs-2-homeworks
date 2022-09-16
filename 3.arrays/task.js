c85ac88292a2098f9223d2ed6eedbc330837450b
Daria Tatarinova <dt.dashenka@mail.ru>
2022-09-16 18:18:51 +1000

    first commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,18 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
-
-  return result; // boolean
+  if (arr1.length === arr2.length) {
+    result = arr1.every((n,i) => n === arr2[i]); // boolean
+    return result;
+  } else {
+    result = false;
+    return result;
+  }
 }
 
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  resultArr = arr.filter(element => Math.sign(element) === 1).filter(element => element % 3 === 0).map(element => element*10);
 
   return resultArr; // array
 }
