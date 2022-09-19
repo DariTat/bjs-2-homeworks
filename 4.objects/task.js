b39b7d9a57201d777be304b94e9e874bdf306fb7
Daria Tatarinova <dt.dashenka@mail.ru>
2022-09-19 23:06:17 +1000

    first commit

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,35 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 }
+let student1 = new Student('Anton', 'male', 39);
+let student2 = new Student('Anna', 'female', 26); 
 
-Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+Student.prototype.setSubject = function(subjectName) {
+  this.subject = subjectName;
 }
+Student.prototype.addMark = function(mark) {
 
-// ваш код для остальных методов
+  if(this.marks === undefined){
+    this.marks = [mark]; 
+  } else {
+    this.marks.push(mark);
+  }
+}
+Student.prototype.addMarks = function(...arr){
+  this.marks = [...arr];
+}
+Student.prototype.getAverage = function(marks){
+  let sum = 0;
+  for (let i = 0; i < this.marks.length; i++){
+    sum += this.marks[i];
+  }
+  this.Average = sum/this.marks.length;
+  return this.Average;
+}
+Student.prototype.exclude = function(reason){
+  delete Student.prototype.addMark;
+  delete Student.prototype.setSubject;
+  this.excluded = reason;
+}
